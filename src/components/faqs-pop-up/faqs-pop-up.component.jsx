e7baf23be33fd78bd7bf0034471bb36f79209cfd
Prateek Goyal <gprateek015@gmail.com>
2022-03-05 18:39:28 +0530

    changed faqs pop up styles

---
+++ src/components/faqs-pop-up/faqs-pop-up.component.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+import './faqs-pop-up.styles.scss';
+
+const data = [
+  {
+    question: 'What is your first question sjk ad biwq bdjw qnki nakdx adkn',
+    answer: 'Answer no.1 nhi pata'
+  },
+  {
+    question:
+      'What is your second question sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx adkn',
+    answer: 'Answer no.2 nhi pata sjk ad biwq bdjw qnki nakdx adkn'
+  },
+  {
+    question: 'What is your third question sjk ad biwq bdjw qnki nakdx adkn',
+    answer:
+      'Answer no.3 nhi pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx adkn'
+  },
+  {
+    question: 'What is your third question sjk ad biwq bdjw qnki nakdx adkn',
+    answer:
+      'Answer no.3 nhi pata sjk ad biwq bdjw qnki nakdx adkn lauran dg di sakl dahkhddhdalk dahdh'
+  },
+  {
+    question: 'What is your third question sjk ad biwq bdjw qnki nakdx adkn',
+    answer:
+      'Answer no.3 nhi pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw'
+  }
+];
+const FAQs = () => {
+  const [selected, setSelected] = useState(null);
+  const toggle = i => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+    return setSelected(i);
+  };
+  return (
+    <div className='faq-pop-up'>
+      {data.map((item, i) => (
+        <div className='item'>
+          <div className='Heading' onClick={() => toggle(i)}>
+            <h3>{item.question}</h3>
+
+            <span>{selected === i ? '-' : '+'}</span>
+          </div>
+          <div className={selected === i ? 'description_all' : 'description'}>
+            <div className='checkup' onClick={() => toggle(i)}>
+              {item.answer}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FAQs;
--- src/components/ticket-house-pop-up/ticket-house-pop-up.component.jsx
+++ src/components/ticket-house-pop-up/ticket-house-pop-up.component.jsx
@@ -1,4 +1,4 @@
-import './ticket-house-pop-up.styles.scss';
+import FAQs from '../faqs-pop-up/faqs-pop-up.component';
 
 import ahd from '../../assets/ahd.png';
 import bang from '../../assets/bang.png';
@@ -9,59 +9,8 @@ import kolk from '../../assets/kolk.png';
 import mumbai from '../../assets/mumbai.png';
 import ncr from '../../assets/ncr.png';
 import pune from '../../assets/pune.png';
-import { useState } from 'react';
 
-const data=[
-  {
-    question: 'What is your first question sjk ad biwq bdjw qnki nakdx adkn',
-    answer: 'Answer no.1 nhi pata',
-  },
-  {
-    question: 'What is your second question sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx adkn',
-    answer: 'Answer no.2 nhi pata sjk ad biwq bdjw qnki nakdx adkn',
-  },
-  {
-    question: 'What is your third question sjk ad biwq bdjw qnki nakdx adkn',
-    answer: 'Answer no.3 nhi pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx adkn',
-  },
-  {
-    question: 'What is your third question sjk ad biwq bdjw qnki nakdx adkn',
-    answer: 'Answer no.3 nhi pata sjk ad biwq bdjw qnki nakdx adkn lauran dg di sakl dahkhddhdalk dahdh',
-  },
-  {
-    question: 'What is your third question sjk ad biwq bdjw qnki nakdx adkn',
-    answer: 'Answer no.3 nhi pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw qnki nakdx pata sjk ad biwq bdjw qnki nakdx adkn sjk ad biwq bdjw',
-  }
-]
-function FAQs() {
-  const [selected,setSelected]= useState(null)
-  const toggle= i=>{
-    if(selected===i)
-    {
-      return  setSelected(null)
-    }
-    return setSelected(i);
-  }
-  return (
-    <div className="faq-pop-up">
-      <div className="line-wise">
-        {data.map((item,i)=>
-        (
-          <div className='item'>
-          <div className='Heading' onClick={()=>toggle(i)}>
-          <h3>{item.question}</h3>
-         
-          <span>{selected===i?'-':'+'}</span>
-          </div>
-          <div className={selected===i?'description_all':'description'}>
-          <div className='checkup'>{item.answer}</div>
-          </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+import './ticket-house-pop-up.styles.scss';
 
 const TicketHousePopUp = ({ PopUp, setPopUp, hoverOn }) => {
   const images = [ahd, bang, chd, hyd, koch, kolk, mumbai, ncr, pune];
