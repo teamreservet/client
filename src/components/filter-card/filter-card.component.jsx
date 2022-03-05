aa166e5973be71fbd6787939be4772f3b7edbe52
Prateek Goyal <gprateek015@gmail.com>
2022-03-05 15:54:17 +0530

    updated ticket house page

--- src/components/filter-card/filter-card.component.jsx
+++ src/components/filter-card/filter-card.component.jsx
@@ -2,15 +2,15 @@ import './filter-card.styles.scss';
 
 import image from '../../assets/grid-1.jpeg';
 
-const FilterCard = ({ name}) => {
-    return (
-        <div className='filter-card'>
-            <img className='filter-img' src={image} alt='' />
-            <div className='text-wrapper'>
-            <h1 className='filter-img-text'>{name}</h1>
-            </div>
-        </div>
-    )
-}
+const FilterCard = ({ name }) => {
+  return (
+    <div className='filter-card'>
+      <img className='filter-img' src={image} alt='' />
+      <div className='text-wrapper'>
+        <h1 className='filter-img-text'>{name.toUpperCase()}</h1>
+      </div>
+    </div>
+  );
+};
 
 export default FilterCard;
